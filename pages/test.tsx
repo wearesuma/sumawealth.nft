3b928f540d87fc27120058f9d32a843d3db0039a
Fady Adal <2masadel@gmail.com>
2022-08-19 00:40:21 +0200

    added header

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,15 +1,18 @@
 import Link from "next/link";
+import Layout from "../components/layout/layout";
 
 import styles from "./test.module.scss";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <iframe src="https://sumawealth.com/home-rent-vs-buy-embed"></iframe>
-      <iframe src="https://sumawealth.com/savings-embed"></iframe>
-      <iframe src="https://sumawealth.com/saving-juntos-embed"></iframe>
-      <iframe src="https://sumawealth.com/credit-card-payoff-embed"></iframe>
-      <iframe src="https://sumawealth.com/incorporate-vs-freelance-embed"></iframe>
-    </div>
+    <Layout title="test badges">
+      <div className={styles.container}>
+        <iframe src="https://sumawealth.com/home-rent-vs-buy-embed"></iframe>
+        <iframe src="https://sumawealth.com/savings-embed"></iframe>
+        <iframe src="https://sumawealth.com/saving-juntos-embed"></iframe>
+        <iframe src="https://sumawealth.com/credit-card-payoff-embed"></iframe>
+        <iframe src="https://sumawealth.com/incorporate-vs-freelance-embed"></iframe>
+      </div>
+    </Layout>
   );
 }
