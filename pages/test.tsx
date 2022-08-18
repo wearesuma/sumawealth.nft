37a1b85e9adec00a7f498d8cb039d5f788011968
Fady Adal <2masadel@gmail.com>
2022-08-19 00:51:13 +0200

    rever header

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,11 +1,14 @@
 import Link from "next/link";
+import Header from "../components/layout/header";
 import Layout from "../components/layout/layout";
 
 import styles from "./test.module.scss";
 
 export default function Home() {
   return (
-    <Layout title="test badges">
+    <div className={styles.all}>
+      <Header />
+      <div className={styles.spacer}></div>
       <div className={styles.container}>
         <iframe src="https://sumawealth.com/home-rent-vs-buy-embed"></iframe>
         <iframe src="https://sumawealth.com/savings-embed"></iframe>
@@ -13,6 +16,6 @@ export default function Home() {
         <iframe src="https://sumawealth.com/credit-card-payoff-embed"></iframe>
         <iframe src="https://sumawealth.com/incorporate-vs-freelance-embed"></iframe>
       </div>
-    </Layout>
+    </div>
   );
 }
